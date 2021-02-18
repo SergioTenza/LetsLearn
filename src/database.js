516c4896eeb0c61bfe5f6da391194c8728dd4cee
Sergio Tenza <43845783+SergioTenza@users.noreply.github.com>
2021-02-18 13:00:44 +0100

    Cleaning prepare to blazor server app

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(db => console.log('DataBase is connected'))
-.catch(err => console.log(err));
